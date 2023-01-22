be8ac4eaab3b2e039117b8e75a8e472df771d6d9
JohnScreamer <bogaturscreamer@gmail.com>
2023-01-22 17:31:24 +0200

    make it better

--- utiles/isPropNull.js
+++
@@ -1,9 +0,0 @@
-export function isPropNull(obj) {
-    const rez = {};
-    Object.keys(obj).forEach((el) => {
-        if (obj[el]) {
-            rez[el] = obj[el];
-        }
-    });
-    return rez;
-}
